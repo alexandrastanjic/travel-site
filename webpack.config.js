5a0394e0f0dd5eeb1650f480b33fdb5f056d4c7d
Alexandra S <alexandrastanjic@gmail.com>
2021-06-24 23:07:18 +0300

    css architecture/postcss & bem

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,26 @@
-
-const path =  require('path');
+const path = require('path')
 
 const postCSSPlugins = [
+  require('postcss-import'),
   require('postcss-simple-vars'),
   require('postcss-nested'),
   require('autoprefixer')
 ]
+
 module.exports = {
   entry: './app/assets/scripts/App.js',
-  output:{
-    filename: 'bundle.js',
+  output: {
+    filename: 'bundled.js',
     path: path.resolve(__dirname, 'app')
-  }, 
+  },
   mode: 'development',
   watch: true,
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader','css-loader', {loader: 'postcss-loader', options:{plugins: postCSSPlugins}}]
+        use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
       }
     ]
   }
-};
+}
